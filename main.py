69ee663c8b692161af81dff48361c53e5ea701b3
KadriR-hash <kadriraed11@gmail.com>
2022-05-06 19:52:42 +0100

    last commit hopefully

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-
 import pandas
 import pandas as pd
 from sklearn import preprocessing, cluster
@@ -15,7 +14,9 @@ corona = pd.read_csv("corona_morocco.csv")
 print(corona)
 print("-------------------------------------------------------------------")
 print(corona.describe())
+print("-------------------------------------------------------------------")
 
+# Selection des variables
 x = corona.iloc[:, 1:].values
 print("X :")
 print(x)
@@ -25,10 +26,12 @@ print(y)
 corr = corona.corr()
 print("Matrice de corrolation:")
 print(corr)
+
 # Centralisation et réduction
 coronacr = preprocessing.scale(x)
 print("variables centrés et réduites :")
 print(coronacr)
+
 # Vecteurs et valeurs propres
 eig_vals, eig_vecs = np.linalg.eig(corr)
 print("Vect  propres :")
@@ -37,22 +40,28 @@ print("Val  propres :")
 print(eig_vals)
 
 print("---------------PART II---------------------------------------------------------------")
-z = linkage(coronacr, method="ward", metric="euclidean")
-dendrogram(z, labels=corona.index, orientation="left", color_threshold=27)
-plt.show()
-
-groupes_cah = fcluster(z, t=27, criterion="distance")
-print(groupes_cah)
-# Observations + groupes
-idg = np.argsort(groupes_cah)
-print(pandas.DataFrame(corona.index[idg], groupes_cah[idg]))
+# =====================CAH==================#
+# matrice des liens + affichage
+# materialisation 4 classes
+#z = linkage(coronacr, method="ward", metric="euclidean")
+#dendrogram(z, labels=corona.index, orientation="left", color_threshold=27)
+#plt.show()
+# Decoupage hauteur 27
+#groupes_cah = fcluster(z, t=27, criterion="distance")
+#print(groupes_cah)
+# tri index + Observations + groupes
+#idg = np.argsort(groupes_cah)
+#print(pandas.DataFrame(corona.index[idg], groupes_cah[idg]))
 
 # =====================KMEANS AVEC DONNEE CENTRALISE REDUITE==================#
 
 kmeans = cluster.KMeans(n_clusters=4)
 kmeans.fit(coronacr)
-
+# Tri index des groupes
 idk = np.argsort(kmeans.labels_)
-print(pandas.DataFrame(corona.index[idg], groupes_cah[idg]))
+# Affichage observations et groupes
+print(pandas.DataFrame(corona.index[idk], kmeans.labels_[idk]))
+# Distances aux centres de classes
 print(kmeans.transform(coronacr))
-pandas.crosstab(groupes_cah, kmeans.labels_)
+# correspondace
+#pandas.crosstab(groupes_cah, kmeans.labels_)
